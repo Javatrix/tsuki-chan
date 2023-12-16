f24054b5addc979ff10f7488be061a2f4408d804
Javatrix <108270039+Javatrix@users.noreply.github.com>
2023-12-16 09:56:20 +0100

    Copyright Update.

--- src/main/java/com/github/javatrix/kawaiisanbot/KawaiiSan.java
+++ src/main/java/com/github/javatrix/kawaiisanbot/KawaiiSan.java
@@ -1,6 +1,7 @@
 /*
  * This file is part of Kawaii-San Discord bot project (https://github.com/Javatrix/kawaiisanbot).
- * Copyright (c) 2023 Javatrix.
+ * Copyright (c) 2023-2023 Javatrix.
+ * The project license can be seen here: https://raw.githubusercontent.com/Javatrix/kawaiisanbot/main/LICENSE
  */
 
 package com.github.javatrix.kawaiisanbot;
